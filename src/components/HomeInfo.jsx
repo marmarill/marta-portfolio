47f1febf7522a449f8f644057b9d3c8cc228ea9a
Marta Locmele <martamarijalocmele@gmail.com>
2024-04-22 20:55:24 +0300

    first commit

---
+++ src/components/HomeInfo.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import { arrow } from '../assets/icons'
+
+const InfoBox = ({ text, link, btnText }) => (
+  <div className='info-box'>
+    <p className='font-medium sm:text-xl text-center'>{text}</p>
+    <Link to={link} className='neo-brutalism-white neo-btn'>
+      {btnText}
+      <img src={arrow} className='w-4 h-4 object-contain' />
+    </Link>
+  </div>
+)
+
+const renderContent = {
+  1: (
+    <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
+      Hi, i am Marta!
+      <br />
+      An aspiring frontend developer
+    </h1>
+  ),
+  2: (
+    <InfoBox
+      text="Self taught frontend developer"
+      link="/about"
+      btnText="Learn more"
+    />
+  ),
+  3: (
+    <h1>3</h1>
+  ),
+  4: (
+    <h1>4</h1>
+  )
+}
+
+
+const HomeInfo = ({ currentStage }) => {
+  return renderContent[currentStage] || null
+}
+
+export default HomeInfo
---
+++ src/components/Loader.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react'
+
+const Loader = () => {
+
+  useEffect(() => {
+    console.log('LOADER STARTED')
+    return () => {
+      console.log('LOADER UNMOUNT')
+    }
+  }, [])
+  return (
+    <div className="flex justify-center items-center bg-black h-full w-full">
+      <div className="w-20 h-20 border-opacity-20 border-blue-500 border-t-blue-500 rounder-full animate-spin">
+
+        asdasdasdas
+      </div>
+    </div>
+  )
+}
+
+export default Loader
