4c050df08c058a883c1a0e28549a4d2faf495b79
Marta Locmele <martamarijalocmele@gmail.com>
2024-05-21 13:52:49 +0300

    styled contact form

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -28,16 +28,16 @@ export const Contact = () => {
         <h1 className='mb-8'>Contact me</h1>
         <div className='flex flex-col content-between'>
           <div className='flex flex-row mb-8'>
-            <input type="text" name="user_name" placeholder='Name' className='p-2 text-2xl  text-black rounded-sm max-w-72 shadow-inner shadow-slate-600' />
+            <input type="text" name="user_name" placeholder='Name' className='p-2 text-xl  text-black rounded-sm max-w-52 shadow-inner shadow-slate-600' />
           </div>
           <div className='flex flex-row mb-8'>
-            <input type="email" name="user_email" placeholder='E-mail' className='p-2 text-2xl text-black rounded-sm max-w-72 shadow-inner shadow-slate-600' />
+            <input type="email" name="user_email" placeholder='E-mail' className='p-2 text-xl text-black rounded-sm max-w-52 shadow-inner shadow-slate-600' />
           </div>
           <div className='flex flex-row mb-8 '>
-            <textarea name="message" placeholder='Message' className=' p-2 text-2xl text-black rounded-sm max-w-72 shadow-inner shadow-slate-600' />
+            <textarea name="message" placeholder='Message' className=' p-2 text-xl text-black rounded-sm max-w-52 shadow-inner shadow-slate-600 focus: border-none' />
           </div>
         </div>
-        <input type="submit" value="Send" />
+        <input type="submit" value="Send" className='cursor-pointer hover:text-slate-300 transition ease-in-out duration-300' />
       </div>
     </form>
   );
