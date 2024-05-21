4dfd938442389e95d8ef5b5fde884a02660e6d8f
Marta Locmele <martamarijalocmele@gmail.com>
2024-05-21 13:42:43 +0300

    fixed contact form

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -25,20 +25,19 @@ export const Contact = () => {
   return (
     <form ref={form} onSubmit={sendEmail}>
       <div className='flex items-center flex-col mb-8'>
-        <h1>Contact me</h1>
-        <div className='flex flex-row mb-8'>
-          <label>Name</label>
-          <input type="text" name="user_name" />
+        <h1 className='mb-8'>Contact me</h1>
+        <div className='flex flex-col content-between'>
+          <div className='flex flex-row mb-8'>
+            <input type="text" name="user_name" placeholder='Name' className='p-2 text-2xl  text-black rounded-sm max-w-72 shadow-inner shadow-slate-600' />
+          </div>
+          <div className='flex flex-row mb-8'>
+            <input type="email" name="user_email" placeholder='E-mail' className='p-2 text-2xl text-black rounded-sm max-w-72 shadow-inner shadow-slate-600' />
+          </div>
+          <div className='flex flex-row mb-8 '>
+            <textarea name="message" placeholder='Message' className=' p-2 text-2xl text-black rounded-sm max-w-72 shadow-inner shadow-slate-600' />
+          </div>
         </div>
-        <div className='flex flex-row mb-8'>
-          <label>Email</label>
-          <input type="email" name="user_email" />
-        </div>
-        <div className='flex flex-row mb-8'>
-          <label>Message</label>
-          <textarea name="message" />
-        </div>
-        <input className="sendButton" type="submit" value="Send" />
+        <input type="submit" value="Send" />
       </div>
     </form>
   );
