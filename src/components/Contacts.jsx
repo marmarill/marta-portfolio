236c8a62afb9add74ec713800d7158dc94385529
Marta Locmele <martamarijalocmele@gmail.com>
2024-06-06 16:16:44 +0300

    fixed contact form

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -53,7 +53,7 @@ export const Contact = () => {
             <Input type={"email"} name={"user_email"} placeholder={"E-mail"} />
           </div>
           <div className='flex flex-row mb-8 '>
-            <textarea name="message" placeholder='Message' className=' text-xl text-white max-w-52 bg-transparent box-border  border-b-2 border-white ' />
+            <textarea name="message" placeholder='Message' className=' text-xl text-white max-w-52 bg-transparent box-border border-b-2 border-white  placeholder:pt-4 placeholder:pl-2 ' />
           </div>
         </div>
         <input type="submit" value="Send" className='cursor-pointer text-xl hover:text-slate-300 transition ease-in-out duration-300' />
